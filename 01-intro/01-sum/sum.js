5631b29a2800a5adc5e0b39c6bcbda57763d4724
matmotex <khf171@khf.ru>
2022-11-27 18:23:21 +1000

    task 1 complete

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,15 @@
+function isNumber(value) {
+  if (typeof(value) != 'number')
+    return false;
+  
+  return !isNaN(Number(value));
+}
+
 function sum(a, b) {
-  /* ваш код */
+  if ( !isNumber(a) || !isNumber(b))
+    throw new TypeError('Не ломай, мамкин хакер :)');
+  
+  return a + b;
 }
 
 module.exports = sum;
