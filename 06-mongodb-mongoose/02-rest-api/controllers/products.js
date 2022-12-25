5cb502a7d5231cbbf78a97b7e7631faf2b7166bf
matmotex <khf171@khf.ru>
2022-12-25 16:28:29 +1000

    task 6 (1 and 2)

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,39 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
+const modelProduct = require('../models/Product');
+const mapperProduct = require('../mappers/product.js');
+const { default: mongoose } = require('mongoose');
+
+/**
+ * Функция возвращает список продуктов
+ * @param {obj} subcategory подкатегория (Необязательно)
+**/
+async function getProducts(subcategory) {
+  return (await modelProduct.find((subcategory == null) ? {} : {subcategory: subcategory})).map(mapperProduct);
+}
+
+
+module.exports.products = async function products(ctx, next) {
   const {subcategory} = ctx.query;
+  const products = await getProducts(subcategory);
+  ctx.body = {products: products};
+}
 
-  if (!subcategory) return next();
-
-  ctx.body = {};
-};
-
-module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
-};
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  //console.dir(ctx.params);
+  const id = ctx.params.id;
+  
+  if (!mongoose.isValidObjectId(id)) {
+    ctx.throw(400, 'invalid id');
+    return;
+  }
+
+  const product = await modelProduct.findById(id);
+  
+  if (product == null) {
+    ctx.throw(404, 'id not found');
+    return;
+  }
+  
+  ctx.body = {product: mapperProduct(product)};
 };
 
