1f607130c5532e8debe3ab0f40bddc6c2525a0e6
matmotex <khf171@khf.ru>
2022-12-25 18:58:55 +1000

    task 6 (3)

--- 06-mongodb-mongoose/03-search/models/Product.js
+++ 06-mongodb-mongoose/03-search/models/Product.js
@@ -32,4 +32,18 @@ const productSchema = new mongoose.Schema({
 
 });
 
+
+productSchema.index(
+  {
+    title: 'text'
+    , description: 'text'
+  }
+  , {
+    name: 'TextSearchIndex',
+    weights: {title: 10, description: 5},
+    default_language: 'russian',
+  }
+);
+
+
 module.exports = connection.model('Product', productSchema);
